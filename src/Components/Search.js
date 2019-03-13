d2b9848a89cbc5ebeb150991ed402bfe8a101320
Tùng Anh Nguyễn <tunganh2521999@gmail.com>
2019-03-13 18:38:37 +0700

    Fix

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -6,7 +6,7 @@ class Search extends Component {
             < div className="col-xs-6 col-sm-6 col-md-6 col-lg-6" >
                 <div className="input-group"><input type="text" name="keyword" className="form-control"
                     placeholder="Nhập từ khóa..." /><span className="input-group-btn"><button className="btn btn-primary"
-                        type="button"><span className="fa fa-search mr-5"></span>Tìm</button></span></div>
+                        type="button"><span className="fa fa-search mr-5"></span>Tìm kiếm</button></span></div>
             </div >
         );
     }
